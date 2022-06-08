7ba1f0754312ff8752f35ac2188ab2a5d89494c7
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-08 22:27:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+var num1 = 2;
+var num2 = 31;
+var multiply = num1*num2;
+
+var random = Math.floor(Math.random() +1);
+var num3 = 24;
+var num4 = 5;
+var mod = num3 % num4;
+
+var max = Math.max(15, 16, 17, 18, 19, 20);
